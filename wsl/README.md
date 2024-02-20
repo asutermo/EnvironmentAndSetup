1bf592da47eb505937c53ca2c86b501de3f91e09
Andrew Suter-Morris <asutermo@users.noreply.github.com>
2024-02-20 08:44:53 -0700

    initial checkin of some tweaks made

---
+++ wsl/README.md
@@ -0,0 +1,10 @@
+# System Tweaks
+
+## ML Tweaks
+
+I needed additional RAM support for WSL. To do this, create a .wslconfig file, and drop it in %USERPROFILE%. Then reboot wsl with:
+
+```powershell
+wsl --shutdown
+```
+
---
+++ wsl/cuda.sh
@@ -0,0 +1,4 @@
+# More docs to come
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/home/asutermo/local/cuda-11.7/lib64
+export PATH=$PATH:/home/asutermo/local/cuda-11.7/bin
+export LD_LIBRARY_PATH=/usr/lib/wsl/lib:$LD_LIBRARY_PATH
